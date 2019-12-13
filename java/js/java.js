44c23a19abfa5670fa3a385b498c732b898d154c
MaysShtanwi <57030254+MaysShtanwi@users.noreply.github.com>
2019-12-13 20:54:26 +0200

    javascript

--- java/js/java.js
+++ java/js/java.js
@@ -1,1 +1,8 @@
 /*alert("hello world!");*/
+// document.getElementById("green").style.backgroundColor= "lightgreen";
+// let i=0;
+// while( i<=100)
+// {
+//  document.getElementById("green").innerHTML +=i + "<br>";
+//  i+=2;
+// }
